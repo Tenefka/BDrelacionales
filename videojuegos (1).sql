8b1a0107666cd3f98992d6e844ce2e5f9ed17c08
Tenefka <jose.leon24@cetis107.edu.mx>
2026-04-29 08:13:23 -0700

    Add files via upload

---
+++ videojuegos (1).sql
@@ -0,0 +1,138 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 29-04-2026 a las 17:12:49
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `videojuegos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `genero`
+--
+
+CREATE TABLE `genero` (
+  `id_genero` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `descripcion` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `genero`
+--
+
+INSERT INTO `genero` (`id_genero`, `nombre`, `descripcion`) VALUES
+(1, 'Simulación', 'Pensados en replicar actividades o situaciones de la vida real'),
+(2, 'Roguelike', 'niveles generados al azar donde la muerte es permanente una muerte y se empieza desde cero'),
+(3, 'bullet hell', 'disparos y objetivos por toda la pantalla, sobrevives a base de constancia');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `juego_plataforma`
+--
+
+CREATE TABLE `juego_plataforma` (
+  `id_juego` int(11) NOT NULL,
+  `id_plataforma` int(11) NOT NULL,
+  `fecha_disponible` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `plataforma`
+--
+
+CREATE TABLE `plataforma` (
+  `id_plataforma` int(11) NOT NULL,
+  `nombre` varchar(60) NOT NULL,
+  `fabricante` varchar(80) NOT NULL,
+  `anio_lanzamiento` year(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `videojuego`
+--
+
+CREATE TABLE `videojuego` (
+  `id_juego` int(11) NOT NULL,
+  `titulo` varchar(120) NOT NULL,
+  `desarrolladora` varchar(100) NOT NULL,
+  `anio_lanzamiento` year(4) NOT NULL,
+  `precio` decimal(10,0) NOT NULL,
+  `id_genero` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `genero`
+--
+ALTER TABLE `genero`
+  ADD PRIMARY KEY (`id_genero`);
+
+--
+-- Indices de la tabla `juego_plataforma`
+--
+ALTER TABLE `juego_plataforma`
+  ADD KEY `id_juego` (`id_juego`,`id_plataforma`);
+
+--
+-- Indices de la tabla `plataforma`
+--
+ALTER TABLE `plataforma`
+  ADD PRIMARY KEY (`id_plataforma`);
+
+--
+-- Indices de la tabla `videojuego`
+--
+ALTER TABLE `videojuego`
+  ADD PRIMARY KEY (`id_juego`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `genero`
+--
+ALTER TABLE `genero`
+  MODIFY `id_genero` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `plataforma`
+--
+ALTER TABLE `plataforma`
+  MODIFY `id_plataforma` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `videojuego`
+--
+ALTER TABLE `videojuego`
+  MODIFY `id_juego` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
